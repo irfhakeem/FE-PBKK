91f44d8a76caa3ef69aea0bcf590b4472b281b6f
IrfanHakimm <mirfanhakim08@gmail.com>
2024-10-27 23:06:08 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheWelcome from "../components/TheWelcome.vue";
+</script>
+
+<template>
+  <main>
+    <div class="flex items-center justify-center font-bold text-3xl h-screen">
+      <h1>Testing</h1>
+    </div>
+  </main>
+</template>
