6b58b3e65b3fb247abdd6cd3bf7796de547f790a
IrfanHakimm <mirfanhakim08@gmail.com>
2024-10-28 15:44:04 +0700

    update: add tailwindcss and shadcn-vue conf

---
+++ src/components/Home.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { Button } from "@/components/ui/button";
+import Navbar from "@/components/Navbar.vue";
+</script>
+
+<template>
+  <Navbar />
+  <div class="bg-black w-full"></div>
+</template>
