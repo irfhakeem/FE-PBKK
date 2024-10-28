4296c4a784162430122fef36c8d817b52eca12e3
IrfanHakimm <mirfanhakim08@gmail.com>
2024-10-29 00:45:40 +0700

    feat: add slider

---
+++ src/components/Slider.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { defineProps, defineEmits } from "vue";
+
+const props = defineProps({
+  categories: {
+    type: Array,
+    required: true,
+  },
+  activeCategory: {
+    type: String,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["update:activeCategory"]);
+
+const updateCategory = (category) => {
+  emit("update:activeCategory", category);
+};
+</script>
+
+<template>
+  <div class="sticky top-0 bg-white">
+    <div class="relative mb-10 py-4 border-b-[2px] border-gray-100">
+      <a
+        v-for="category in categories"
+        :key="category"
+        :class="[
+          'mr-10 pb-4 text-sm hover:text-black cursor-pointer',
+          activeCategory === category
+            ? 'border-b-2 border-black text-black'
+            : 'text-gray-500',
+        ]"
+        @click="updateCategory(category)"
+        style="white-space: nowrap"
+      >
+        {{ category }}
+      </a>
+    </div>
+  </div>
+</template>
