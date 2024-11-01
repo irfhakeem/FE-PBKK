66aa455ec9b9e7919fa43c32a68f2d0834ad4afe
steven <steven.leka12@gmail.com>
2024-11-01 21:06:19 +0700

    modified:   src/views/Setting.vue

--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -47,141 +47,72 @@ console.log(recommendedPosts.value);
 
 <template>
   <Navbar :user-username="user.username" :user-photo="user.avatar" />
+  <div class="flex flex-col min-h-screen">
+  <!-- Header -->
+    <header class ="grid grid-cols-1 py-5 px-10">
+      <div class="container px-4">
+        <!-- Header content -->
+        <div class="grid grid-cols-1 py-5 px-10 ">
+          <h1 class="text-3xl font-semibold">Settings</h1>
+        </div>
 
-  <div class="grid grid-cols-1 mx-auto py-5 px-10 ">
-    <h1 class="text-3xl font-semibold mb-6">Settings</h1>
+        <!-- navigation menu -->
+        <div class="grid grid-cols-1 py-5 px-10 ">
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger class="font-semibold">Account</NavigationMenuTrigger>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+      </div>
+    </header>
+
+    <!-- Main Content -->
+    <div class="container grid grid-cols-1 lg:max-w-10xl py-5 px-10 flex-grow">
+      <!-- content -->
+      <div class="grid grid-cols-1 lg:max-w-10xl py-5 px-10">
+      <!-- Account Section -->
+        <div class="border-b pb-4 mb-6">
+          <div class="flex justify-between items-center mt-4 mb-4 gap-3">
+            <span>Email address</span>
+            <span class="text-gray-600">mirfanhakim1004@gmail.com</span>
+          </div>
+          <div class="flex justify-between items-center mt-4 mb-4 gap-3">
+            <span>Username and subdomain</span>
+            <span class="text-gray-600">@mirfanhakim100</span>
+          </div>
+          <div class="flex justify-between items-center mt-4 mb-4 gap-3">
+            <span>Profile information</span>
+            <span class="text-gray-600">Muhammad Irfan Hakim</span>
+          </div>
+          <div class="flex justify-between items-center mt-4 mb-4 gap-3">
+            <span>Change password</span>
+            <span class="text-gray-600">Change your password.</span>
+          </div>
+        </div>
+
+        <div class="border-b pb-4 mb-6">
+          <div class="flex justify-between items-center mt-4 mb-4 gap-3">
+            <span>Delete account</span>
+            <span class="text-gray-600">Delete this account.</span>
+          </div>
+        </div>
+      </div>
+   </div>
   </div>
 
-  <!-- navigation menu -->
-
-  <NavigationMenu>
-    <NavigationMenuList>
-      <NavigationMenuItem>
-        <NavigationMenuTrigger>Account</NavigationMenuTrigger>
-        <NavigationMenuContent>
-          <NavigationMenuLink>Link</NavigationMenuLink>
-        </NavigationMenuContent>
-      </NavigationMenuItem>
-    </NavigationMenuList>
-  </NavigationMenu>
+  
 
   <!-- <div class="grid grid-cols-1 mx-auto py-3 px-10 ">
     <h2 class="text-xl font-semibold mb-2">Account</h2>
   </div> -->
   
-  <!-- content -->
-  <div class="grid grid-cols-1 mx-auto py-5 px-10">
-    Content here
-    <!-- Account Section -->
-    <div class="border-b pb-4 mb-6">
-      <div class="flex justify-between items-center mb-2">
-        <span>Email address</span>
-        <span class="text-gray-600">mirfanhakim1004@gmail.com</span>
-      </div>
-      <div class="flex justify-between items-center mb-2">
-        <span>Username and subdomain</span>
-        <span class="text-gray-600">@mirfanhakim100</span>
-      </div>
-      <div class="flex justify-between items-center mb-2">
-        <span>Profile information</span>
-        <span class="text-gray-600">Muhammad Irfan Hakim</span>
-      </div>
-      <div class="flex justify-between items-center mb-2">
-        <span>Profile design</span>
-        <span class="text-gray-600">Customize the appearance of your profile.</span>
-      </div>
-      <div class="flex justify-between items-center mb-2">
-        <span>Custom domain</span>
-        <span class="text-gray-600">None</span>
-      </div>
-      <div class="flex justify-between items-center mb-2">
-        <span>Partner Program</span>
-        <span class="text-gray-600">You are not enrolled in the Partner Program</span>
-      </div>
-      <div class="flex justify-between items-center mb-2">
-        <span>Muted writers and publications</span>
-        <span class="text-gray-600">N/A</span>
-      </div>
-      <div class="flex justify-between items-center mb-2">
-        <span>Blocked users</span>
-        <span class="text-gray-600">N/A</span>
-      </div>
-    </div>
-  </div>
+ 
+
 
   <!-- footer -->
-  <div class="bg-[#f7f7f7]">
-    <div class="flex flex-col mx-auto lg:max-w-3xl p-5">
-      <div className="flex flex-col mt-5">
-        <img
-          :src="author.avatar"
-          alt=""
-          className="w-20 h-20 rounded-full mb-4 object-cover"
-        />
-        <div className="flex justify-between items-start mb-2 gap-7">
-          <div className="flex flex-col">
-            <a href="" className="font-semibold text-2xl"
-              >Written by {{ author.name }}</a
-            >
-
-            <p className="text-sm text-black mb-2">100 followers</p>
-            <p className="text-sm text-black ">{{ author.bio }}</p>
-          </div>
-          <div className="flex gap-2">
-            <FollowButton
-              custom="font-medium bg-[#37823a] hover:bg-[#295d2a] text-[#f7f7f7] rounded-full px-4 py-[8px] text-[14px]"
-              authorId="{author.id}"
-              currentUserId="{currentUserId}"
-              followUser="{follows}"
-            />
-            <button
-              className="font-medium bg-[#37823a] hover:bg-[#295d2a] text-[#f7f7f7] rounded-full px-2 py-[8px] text-[14px]"
-            >
-              <Bell aria-hidden="true" />
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-col gap-7">
-        <div
-          className="grid-flow-row items-center border-y-[1px] border-gray-200"
-        >
-          <p className="text-base text-black font-semibold my-10">
-            More from {{ author.name }}
-          </p>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-7 mb-5">
-            <div v-for="recommendedPost in recommendedPosts" :key="post.id">
-              <div class="flex flex-col gap-5">
-                <img
-                  :src="recommendedPost.image"
-                  alt=""
-                  className="w-auto h-48 object-cover"
-                />
-                <ProfileCard :author="author" :userID="user.id" />
-                <div class="flex flex-col gap-2">
-                  <a
-                    :href="'/' + author.username + '/' + recommendedPost.id"
-                    className="text-lg font-bold text-black line-clamp-2"
-                  >
-                    {{ recommendedPost.title }}
-                  </a>
-                  <p className="text-gray-500 text-sm line-clamp-2">
-                    {{ recommendedPost.caption }}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div>
-          <a
-            :href="'/profile/' + author.username"
-            className="border-[1px] rounded-full border-black text-black text-sm font-medium py-2 px-4 hover:bg-black hover:text-white"
-          >
-            See more from {{ author.name }}
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
+  
+ 
 </template>
