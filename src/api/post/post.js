c635550de1f3fab5b7e3cf6e351fcd6f57b8e2be
IrfanHakimm <mirfanhakim08@gmail.com>
2024-11-05 22:36:51 +0700

    feat: add fetching like API function

--- src/api/post/post.js
+++ src/api/post/post.js
@@ -46,3 +46,32 @@ export const getPosts = async (data) => {
     throw new Error("Failed to fetch posts data");
   }
 };
+
+export const likePost = async (data) => {
+  try {
+    const response = await apiClient.post("/like", data);
+    return response.data.data;
+  } catch (error) {
+    throw new Error("Failed to like post");
+  }
+};
+
+export const unlikePost = async (postId) => {
+  try {
+    const response = await apiClient.delete("/unlike", {
+      data: { postId },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to unlike post");
+  }
+};
+
+export const isLiked = async (data) => {
+  try {
+    const response = await apiClient.post("/is-liked", data);
+    return response.data.data;
+  } catch (error) {
+    throw new Error("Failed to check if post is liked");
+  }
+};
