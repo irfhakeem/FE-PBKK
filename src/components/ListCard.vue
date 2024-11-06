52b9327546f5801fdf94b0d8a3ea5e50cb28a397
IrfanHakimm <mirfanhakim08@gmail.com>
2024-11-06 19:29:56 +0700

    fix: show 3 posts image

--- src/components/ListCard.vue
+++ src/components/ListCard.vue
@@ -16,6 +16,7 @@ import { userByUsername } from "@/api/user/user.js";
 import { ref, onMounted, watch } from "vue";
 import { Ellipsis } from "lucide-vue-next";
 import { useRouter } from "vue-router";
+import { deleteList } from "@/api/user/list.js";
 
 const router = useRouter();
 const props = defineProps({
@@ -24,6 +25,9 @@ const props = defineProps({
 });
 
 const author = ref({});
+const list = ref(props.list);
+
+console.log(list.value);
 
 const fetchAuthor = async () => {
   if (props.authorUsername) {
@@ -39,11 +43,23 @@ const navigateToLink = () => {
     path: `/profile/${author.value.username}/lists/${props.list.id}`,
   });
 };
+
+const deleteListHandler = async () => {
+  await deleteList(list.value.id);
+  router.go(0);
+};
+
+const handleCopyLink = () => {
+  navigator.clipboard.writeText(
+    `http://localhost:5173/profile/${author.value.username}/lists/${props.list.id}`
+  );
+};
 </script>
 
+<!-- Delete List -->
 <template>
   <Card class="w-full cursor-pointer mb-5">
-    <div class="flex">
+    <div class="flex flex-row justify-between">
       <!-- sections -->
       <div class="flex-[0.55] bg-gray-100">
         <CardHeader @click="navigateToLink">
@@ -78,28 +94,54 @@ const navigateToLink = () => {
               </DropdownMenuTrigger>
               <DropdownMenuContent class="md:w-40 sm:w-48 w-40">
                 <DropdownMenuItem class="sm:text-sm text-xs"
-                  >Copy link</DropdownMenuItem
+                  ><button @click="handleCopyLink">
+                    Copy link
+                  </button></DropdownMenuItem
                 >
                 <!-- <div v-if=""></div> -->
                 <DropdownMenuItem class="sm:text-sm text-xs"
                   >Edit list info</DropdownMenuItem
                 >
                 <DropdownMenuItem class="sm:text-sm text-xs"
-                  >Remove items</DropdownMenuItem
+                  ><button @click="deleteListHandler">
+                    Remove items
+                  </button></DropdownMenuItem
                 >
               </DropdownMenuContent>
             </DropdownMenu>
           </div>
         </CardFooter>
       </div>
-      <div class="flex-[0.25] bg-red-100">
-        <!-- fill with story banner klo ada-->
-      </div>
-      <div class="flex-[0.15] bg-green-100 ml-1">
-        <!-- fill with story banner klo ada-->
-      </div>
-      <div class="flex-[0.06] bg-blue-100 ml-1">
-        <!-- fill with story banner klo ada-->
+
+      <div class="flex-[0.45] flex gap-1">
+        <div
+          v-if="list.posts.length === 0"
+          v-for="i in 3"
+          :key="i"
+          :class="{
+            'flex-[0.5]': i === 1,
+            'flex-[0.3]': i === 2,
+            'flex-[0.2]': i === 3,
+          }"
+        >
+          <div class="flex-1 bg-gray-200 h-full"></div>
+        </div>
+        <div
+          v-else
+          v-for="(post, index) in list.posts.slice(0, 3)"
+          :key="post.id"
+          :class="{
+            'flex-[0.5]': index === 0,
+            'flex-[0.3]': index === 1,
+            'flex-[0.2]': index === 2,
+          }"
+        >
+          <img
+            :src="post.image"
+            alt="Post image"
+            class="h-full w-auto object-cover"
+          />
+        </div>
       </div>
     </div>
   </Card>
